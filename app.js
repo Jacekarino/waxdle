ec5cfae8122cf53b87f1c1d507cdadae5c386bc4
Jacek Kowalczyk <jkowalczyk357@gmail.com>
2026-06-10 02:16:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function hashCode(str) {
 
 function selectTargetAlbum() {
   if (currentMode === 'daily') {
-    const seed = hashCode(getTodayString() + "version3");
+    const seed = hashCode(getTodayString() + "version1");
     return albumsDB[seed % albumsDB.length];
   } else {
     const randomIndex = Math.floor(Math.random() * albumsDB.length);
